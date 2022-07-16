0365cc155b05de5aadcb15ade5bd71d24f5b6377
ueaj <dev.sel20@gmail.com>
2022-07-16 11:58:36 -0600

    fix soft keybind conflict

--- src/main/java/com/kqp/inventorytabs/mixin/KeyBindingMixin_SoftConflict.java
+++ src/main/java/com/kqp/inventorytabs/mixin/KeyBindingMixin_SoftConflict.java
@@ -18,27 +18,47 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
 @Mixin(KeyBinding.class)
-public class KeyBindingMixin_SoftConflict {
+public abstract class KeyBindingMixin_SoftConflict {
 	@Shadow @Final private static Map<String, KeyBinding> KEYS_BY_ID;
 	
 	@Shadow private InputUtil.Key boundKey;
 	
 	@Shadow private int timesPressed;
 	
+	@Shadow public abstract void setPressed(boolean pressed);
+	
+	@Shadow private boolean pressed;
+	
 	@Inject(method = "onKeyPressed", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/KeyBinding;timesPressed:I"),
 			locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
 	private static void onKeyPressed(InputUtil.Key key, CallbackInfo ci, KeyBinding binding) {
+		KeyBindingMixin_SoftConflict alternative = (KeyBindingMixin_SoftConflict) (Object) findAlternative(key, binding, InventoryTabsClient.NEXT_TAB_KEY_BIND);
+		if(alternative != null) {
+			alternative.timesPressed++;
+			ci.cancel();
+		}
+	}
+	
+	@Inject(method = "setKeyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/KeyBinding;setPressed(Z)V"), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+	private static void keyPressed(InputUtil.Key key, boolean pressed, CallbackInfo ci, KeyBinding keyBinding) {
+		KeyBinding alternative = findAlternative(key, keyBinding, InventoryTabsClient.NEXT_TAB_KEY_BIND);
+		if(alternative != null) {
+			alternative.setPressed(pressed);
+			ci.cancel();
+		}
+	}
+	
+	private static KeyBinding findAlternative(InputUtil.Key key, KeyBinding binding, KeyBinding alternativeTo) {
 		Screen screen = MinecraftClient.getInstance().currentScreen;
-		if(binding == InventoryTabsClient.NEXT_TAB_KEY_BIND && !InventoryTabsClient.screenSupported(screen)) {
+		if(binding == alternativeTo && !InventoryTabsClient.screenSupported(screen)) {
 			for(KeyBinding value : KEYS_BY_ID.values()) {
 				KeyBindingMixin_SoftConflict self = (KeyBindingMixin_SoftConflict) (Object) value;
 				InputUtil.Key bound = self.boundKey;
-				if(Objects.equals(bound, key)) {
-					self.timesPressed++;
-					ci.cancel();
-					return;
+				if(Objects.equals(bound, key) && value != alternativeTo) {
+					return value;
 				}
 			}
 		}
+		return null;
 	}
 }
