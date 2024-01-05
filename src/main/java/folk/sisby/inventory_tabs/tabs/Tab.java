c6f3b336dee807f703d97aa18f76eb9a9afbbbd4
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-01-05 16:57:01 +1100

    render tab background by slicing creative tabs adds free texture pack compatibility, but reduces room for adjustment

--- src/main/java/folk/sisby/inventory_tabs/tabs/Tab.java
+++ src/main/java/folk/sisby/inventory_tabs/tabs/Tab.java
@@ -1,7 +1,5 @@
 package folk.sisby.inventory_tabs.tabs;
 
-import folk.sisby.inventory_tabs.InventoryTabs;
-import folk.sisby.inventory_tabs.TabManager;
 import folk.sisby.inventory_tabs.util.WidgetPosition;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -16,7 +14,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
 
 public interface Tab {
-    Identifier TABS_TEXTURE = InventoryTabs.id("textures/gui/tabs.png");
+    Identifier TABS_TEXTURE = new Identifier("textures/gui/container/creative_inventory/tabs.png");
+    int TAB_TEXTURE_WIDTH = 26;
+    int TAB_TEXTURE_HEIGHT = 32;
+    int TAB_TEXTURE_U = 26;
+    int TAB_TEXTURE_V_UNSELECTED = 2;
+    int TAB_TEXTURE_V_SELECTED = 32;
+    int TAB_TEXTURE_V_INVERTED_OFFSET = 64;
 
     /**
      * Opens the screen associated with the tab.
@@ -58,12 +62,12 @@ public interface Tab {
 
     default void renderBackground(DrawContext drawContext, WidgetPosition pos, int width, int height, boolean current) {
         int y = pos.y + (pos.up ? -height + 4 : height - 4);
-        if (!current) drawContext.drawTexture(TABS_TEXTURE, pos.x, y, TabManager.TAB_WIDTH, pos.up ? 0 : (TabManager.TAB_HEIGHT * 2), width, height);
+        if (!current) drawContext.drawNineSlicedTexture(TABS_TEXTURE, pos.x, y, width, height, 6, TAB_TEXTURE_WIDTH, TAB_TEXTURE_HEIGHT, TAB_TEXTURE_U, TAB_TEXTURE_V_UNSELECTED + (pos.up ? 0 : TAB_TEXTURE_V_INVERTED_OFFSET));
     }
 
     default void renderForeground(DrawContext drawContext, WidgetPosition pos, int width, int height, double mouseX, double mouseY, boolean current) {
         int y = pos.y + (pos.up ? -height + 4 : height - 4);
-        if (current) drawContext.drawTexture(TABS_TEXTURE, pos.x, y, TabManager.TAB_WIDTH, TabManager.TAB_HEIGHT + (pos.up ? 0 : (TabManager.TAB_HEIGHT * 2)), width, height);
+        if (current) drawContext.drawNineSlicedTexture(TABS_TEXTURE, pos.x, y, width, height, 6, TAB_TEXTURE_WIDTH, TAB_TEXTURE_HEIGHT, TAB_TEXTURE_U, TAB_TEXTURE_V_SELECTED + (pos.up ? 0 : TAB_TEXTURE_V_INVERTED_OFFSET));
         int itemPadding = Math.max(0, (width - 16) / 2);
         drawContext.drawItem(getTabIcon(), pos.x + itemPadding, y + itemPadding);
         if (new Rect2i(pos.x + itemPadding, y + itemPadding, 16, 16).contains((int) mouseX, (int) mouseY)) {
