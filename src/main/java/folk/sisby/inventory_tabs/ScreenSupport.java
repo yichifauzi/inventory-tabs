4f923622fc4edb17f0e20ce45ad354614896aeb4
Hestia Mae <62225435+HestiMae@users.noreply.github.com>
2024-01-23 11:14:43 +1100

    adds config for left and right tab bounds per screen id (#26)
    fixes #2

--- src/main/java/folk/sisby/inventory_tabs/ScreenSupport.java
+++ src/main/java/folk/sisby/inventory_tabs/ScreenSupport.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.HorseScreen;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +16,7 @@ import java.util.function.Predicate;
 public class ScreenSupport {
     public static Map<Identifier, Predicate<HandledScreen<?>>> DENY = new HashMap<>();
     public static Map<Identifier, Predicate<HandledScreen<?>>> ALLOW = new HashMap<>();
+    public static Map<Identifier, Pair<Integer, Integer>> SCREEN_BOUND_OFFSETS = new HashMap<>();
 
     public static boolean allowTabs(Screen screen) {
         if (screen instanceof HandledScreen<?> hs && hs.getScreenHandler() != null) {
@@ -22,8 +24,10 @@ public class ScreenSupport {
             if (ALLOW.values().stream().anyMatch(p -> p.test(hs))) return true;
             try {
                 Identifier handlerId = Registries.SCREEN_HANDLER.getId(hs.getScreenHandler().getType());
-                if (InventoryTabs.CONFIG.screenOverrides.entrySet().stream().filter(e -> !e.getValue()).anyMatch(e -> Objects.equals(e.getKey(), handlerId.toString()))) return false;
-                if (InventoryTabs.CONFIG.screenOverrides.entrySet().stream().filter(Map.Entry::getValue).anyMatch(e -> Objects.equals(e.getKey(), handlerId.toString()))) return true;
+                if (InventoryTabs.CONFIG.screenOverrides.entrySet().stream().filter(e -> !e.getValue()).anyMatch(e -> Objects.equals(e.getKey(), handlerId.toString())))
+                    return false;
+                if (InventoryTabs.CONFIG.screenOverrides.entrySet().stream().filter(Map.Entry::getValue).anyMatch(e -> Objects.equals(e.getKey(), handlerId.toString())))
+                    return true;
             } catch (UnsupportedOperationException ignored) {
             }
             return InventoryTabs.CONFIG.allowScreensByDefault;
@@ -34,5 +38,7 @@ public class ScreenSupport {
     static {
         DENY.put(InventoryTabs.id("creative_screen"), hs -> hs instanceof CreativeInventoryScreen);
         ALLOW.put(InventoryTabs.id("horse_screen"), hs -> hs instanceof HorseScreen);
+        InventoryTabs.CONFIG.leftBoundOffsetOverride.forEach((screenHandlerId, offset) -> SCREEN_BOUND_OFFSETS.put(screenHandlerId.equals("null") ? null : new Identifier(screenHandlerId), new Pair<>(offset, 0)));
+        InventoryTabs.CONFIG.rightBoundOffsetOverride.forEach((screenHandlerId, offset) -> SCREEN_BOUND_OFFSETS.merge(screenHandlerId.equals("null") ? null : new Identifier(screenHandlerId), new Pair<>(0, offset), (o, n) -> new Pair<>(o.getLeft(), n.getRight())));
     }
 }
